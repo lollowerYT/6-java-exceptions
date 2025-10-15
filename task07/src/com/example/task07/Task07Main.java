b0e8f104556dd94495d0e23d69a0ef90461826da
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 20:37:30 +0500

    Update Task07Main.java

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -7,18 +7,24 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
+        Task07Main main = new Task07Main();
+        
+        main.processor = new Processor();
+        
+        System.out.println("Тип исключения: " + main.getExceptionType());
     }
 
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+            processor.process();
+            return NONE;  // Если исключение не возникло, возвращаем NONE
+        } catch (RuntimeException e) {
+            return UNCHECKED; // Если возникло RuntimeException или его подтип - это unchecked
         } catch (Exception e) {
-
+            return CHECKED;  // Все остальные исключения - checked
         }
-        return null;
     }
-
+}
 }
