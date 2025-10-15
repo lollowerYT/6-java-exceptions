9991f93b2ea7249e363427f2019a3fa269dcc9d9
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 19:42:21 +0500

    Update Task04Main.java

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,17 +1,42 @@
 package com.example.task04;
 
-public class Task04Main {
+// Создаем собственный класс исключения, наследуемый от IllegalArgumentException
+class MyException extends IllegalArgumentException {
+    public MyException(String message) {
+        super(message);
+    }
+}
 
+public class Task04Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(-5));
-         */
+        try {
+            System.out.println(getSeason(1));    // Зима
+            System.out.println(getSeason(3));    // Весна
+            System.out.println(getSeason(6));    // Лето
+            System.out.println(getSeason(9));    // Осень
+            System.out.println(getSeason(12));   // Зима
+            System.out.println(getSeason(-5));   // Исключение
+        } catch (MyException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber < 1 || monthNumber > 12) {
+            throw new MyException(
+                "monthNumber " + monthNumber + " недопустимо, номер месяца должен быть в диапазоне от 1 до 12"
+            );
+        }
+        
+        // Определение сезона по номеру месяца
+        if (monthNumber == 12 || monthNumber <= 2) {
+            return "зима";
+        } else if (monthNumber <= 5) {
+            return "весна";
+        } else if (monthNumber <= 8) {
+            return "лето";
+        } else {
+            return "осень";
+        }
     }
-
 }
