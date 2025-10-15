5799d593408b31edcf1f8982f604c4942667e04b
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 19:54:02 +0500

    Update Task06Main.java

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -2,15 +2,29 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        new Task06Main().printMethodName();
-         */
+        Task06Main task = new Task06Main();
+        
+        // Пример вызова из main
+        task.printMethodName();
+        
+        // Дополнительные методы для демонстрации
+        task.method1();
+        task.method2();
     }
+    
+    // Дополнительные методы для тестирования
+    void method1() {printMethodName();}
+    void method2() {printMethodName();}
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        // Текущий стек вызовов
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        
+        // 0 - всегда указывает на метод getStackTrace()
+        // 1 - указывает на метод printMethodName()
+        // 2 - содержит информацию о методе, который вызвал printMethodName()
+        StackTraceElement callerMethod = stackTrace[2];
 
+        System.out.println("Метод вызван из: " + callerMethod.getMethodName());
+    }
 }
