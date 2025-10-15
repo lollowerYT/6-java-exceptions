c40420c49c7b3182a44f47d7d82d95edfc58054a
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-15 19:47:39 +0500

    Update Task05Main.java

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,28 +2,41 @@ package com.example.task05;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
-        String pathToFile = args[0]; // "/home/user/file.txt"
-
-        String s = readFile(pathToFile);
-        System.out.println(s);
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Необходимо указать путь к файлу");
+            return;
+        }
+        
+        String pathToFile = args[0];
+        
+        try {
+            String s = readFile(pathToFile);
+            System.out.println(s);
+        } catch (FileNotFoundException e) {
+            System.err.println("файл \"" + pathToFile + "\" не найден");
+        } catch (IOException e) {
+            System.err.println("произошла ошибка при чтении файла \"" + pathToFile + "\"");
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
-        FileReader fileReader = new FileReader(pathToFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String currentLine;
-        while ((currentLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append(currentLine);
-            stringBuilder.append("\n");
+        try (FileReader fileReader = new FileReader(pathToFile);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentLine;
+            
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(currentLine);
+                stringBuilder.append("\n");
+            }
+            
+            return stringBuilder.toString();
         }
-        bufferedReader.close();
-
-        return stringBuilder.toString();
     }
 }
